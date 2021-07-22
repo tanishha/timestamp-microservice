ec487e8a17253e98424e3a4889999c52cc13dc88
Tanisha Chaudhary <tanishach203@gmail.com>
2021-07-22 21:53:14 +0545

    date issue handled

--- modules/controller.js
+++ modules/controller.js
@@ -2,13 +2,12 @@ const moment = require("moment");
 
 function date(req, res) {
     const dateFormat = "YYYY-MM-DD";
-    const toDateFormat = moment(new Date(req.params.date)).format(dateFormat);
-
+    const toDateFormat = moment(new Date(req.params.id)).format(dateFormat);
     if (moment(toDateFormat, dateFormat, true).isValid()) {
         var DATE = moment(req.params.date).format('ddd, D MMM YYYY')
         var formatDate = DATE + " " + '00:00:00 GMT'
         res.json({
-            unix: Math.round((new Date(req.params.date)).getTime()),
+            unix: Math.round((new Date(req.params.id)).getTime()),
             utc: formatDate
         })
 
@@ -24,8 +23,6 @@ function date(req, res) {
         res.json({
             error: "Invalid Date"
         })
-        console.log(req.params)
-
     }
 }
 
