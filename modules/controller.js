6d185d6c352618257b050faf3937a68bc7909f47
Tanisha Chaudhary <tanishach203@gmail.com>
2021-07-22 21:46:54 +0545

    unix

--- modules/controller.js
+++ modules/controller.js
@@ -6,19 +6,38 @@ function date(req, res) {
 
     if (moment(toDateFormat, dateFormat, true).isValid()) {
         var DATE = moment(req.params.date).format('ddd, D MMM YYYY')
-        var formatDate=DATE + " "+ '00:00:00 GMT'
+        var formatDate = DATE + " " + '00:00:00 GMT'
         res.json({
-            unix:Math.round((new Date(req.params.date)).getTime()),
+            unix: Math.round((new Date(req.params.date)).getTime()),
             utc: formatDate
         })
 
+    } else if (!isNaN(req.params.id)) {
+        var intId = parseInt(req.params.id)
+        var DATE = moment(intId).format('ddd, D MMM YYYY')
+        var formatDate = DATE + " " + '00:00:00 GMT'
+        res.json({
+            unix: intId,
+            utc: formatDate
+        })
     } else {
         res.json({
             error: "Invalid Date"
         })
+        console.log(req.params)
+
     }
 }
 
+function data(req, res) {
+
+    res.json({
+        unix: Math.round((new Date()).getTime()),
+        utc: new Date().toUTCString()
+    })
+}
+
 module.exports = {
-    date
+    date,
+    data,
 };
--- modules/routes.js
+++ modules/routes.js
@@ -1,6 +1,7 @@
 const Router= require("express").Router();
 const timeCtrl=require('./controller')
 
-Router.route("/:date").get(timeCtrl.date)
+Router.route("/:id").get(timeCtrl.date)
+Router.route("/").get(timeCtrl.data)
 
 module.exports = Router;
