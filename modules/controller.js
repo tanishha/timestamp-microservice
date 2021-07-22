be1aa586c17795bf5993ffc4589adac056ca1d92
Tanisha Chaudhary <tanishach203@gmail.com>
2021-07-22 13:49:53 +0545

    date request

--- modules/controller.js
+++ modules/controller.js
@@ -1,5 +1,24 @@
+const moment = require("moment");
 
+function date(req, res) {
+    const dateFormat = "YYYY-MM-DD";
+    const toDateFormat = moment(new Date(req.params.date)).format(dateFormat);
+
+    if (moment(toDateFormat, dateFormat, true).isValid()) {
+        var DATE = moment(req.params.date).format('ddd, D MMM YYYY')
+        var formatDate=DATE + " "+ '00:00:00 GMT'
+        res.json({
+            unix:Math.round((new Date(req.params.date)).getTime()),
+            utc: formatDate
+        })
+
+    } else {
+        res.json({
+            error: "Invalid Date"
+        })
+    }
+}
 
 module.exports = {
-   
+    date
 };
--- modules/routes.js
+++ modules/routes.js
@@ -1,6 +1,6 @@
 const Router= require("express").Router();
 const timeCtrl=require('./controller')
 
-
+Router.route("/:date").get(timeCtrl.date)
 
 module.exports = Router;
