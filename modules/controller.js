6c0fc5f25132568a24e4ce4dbb8f612216ad63a9
Tanisha Chaudhary <tanishach203@gmail.com>
2021-07-22 23:28:06 +0545

    date issue handled

--- modules/controller.js
+++ modules/controller.js
@@ -2,30 +2,26 @@ const moment = require("moment");
 
 function date(req, res) {
     let date_string = req.params.id;
-     let date;
+    let date;
 
     if (parseInt(date_string) < 10000) {
         date = new Date(date_string);
 
-        var DATE = moment(date).format('ddd, D MMM YYYY')
-        var formatDate = DATE + " " + '00:00:00 GMT'
-        res.json({
-            unix: Math.round((new Date(req.params.id)).getTime()),
-            utc: formatDate
-        })
-
-    } else if (!isNaN(req.params.id)) {
+    } else {
         date = new Date(parseInt(date_string));
+
+    }
+    if (date == "Invalid Date") {
+        return res.json({
+            error: "Invalid Date"
+        })
+    } else {
         var DATE = moment(date).format('ddd, D MMM YYYY')
         var formatDate = DATE + " " + '00:00:00 GMT'
         res.json({
             unix: date.valueOf(),
-            utc: formatDate
-        })
-    } else {
-        res.json({
-            error: "Invalid Date"
-        })
+            utc: formatDate,
+        });
     }
 }
 
